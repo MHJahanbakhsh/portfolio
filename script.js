5d1770c13de25386da377e224406f78ecaa448b9
MohammadHossein Jahanbakhsh <MohammadHosseinJahanbakhsh@hotmail.com>
2021-06-08 15:02:18 +0430

    initial commit with lots of issus

---
+++ script.js
@@ -0,0 +1,38 @@
+//change navbar icon
+
+//elements
+const navIcon=document.getElementById('nav-icon');
+const menu = document.getElementById('menu')
+const intro = document.querySelector('.intro')
+const introP = document.getElementById('par')
+const introH1 = document.getElementById('h1')
+const cards = document.querySelector('.cards-container')
+
+//add event listner
+navIcon.addEventListener('click',toggleFunc);
+navIcon.addEventListener('click',toggleMenu);
+
+
+function toggleFunc(e) {
+    e.target.classList.toggle('fa-times')
+}
+
+function toggleMenu() {
+    menu.classList.toggle('menu-show')
+}
+
+window.addEventListener('load',opacityFunc);
+function opacityFunc() {
+    intro.style.opacity=1;
+    introP.style.transform='translateX(0)';
+    introH1.style.transform='translateX(0)';
+
+}
+
+window.addEventListener('load',showCards);
+
+
+function showCards() {
+       cards.style.transform='scale(1,1)'
+    
+}
