d2abc2a6e82a2b993c3033c4384b95b62a15717b
MohammadHossein Jahanbakhsh <MohammadHosseinJahanbakhsh@hotmail.com>
2021-06-21 13:57:08 +0430

    fix name logo and cards functionality

--- script.js
+++ script.js
@@ -7,13 +7,24 @@ const intro = document.querySelector('.intro')
 const introP = document.getElementById('par')
 const introH1 = document.getElementById('h1')
 const cards = document.querySelectorAll('.card')
+const closePanel = document.querySelectorAll('.close-panel')
+const panel = document.getElementsByClassName('panel')
+const ReadMoreBtn = document.getElementsByClassName('projectbtn')
+const header = document.querySelectorAll('header')[0]
+const main = document.querySelectorAll('main')[0]
+const footer = document.querySelectorAll('footer')[0]
+const svgImage = document.getElementById('svg-image')
+
+console.log(ReadMoreBtn)
 
 //add event listner
-navIcon.addEventListener('click',toggleFunc);
+navIcon.addEventListener('click',toggleIcon);
 navIcon.addEventListener('click',toggleMenu);
+// navIcon.addEventListener('click',closeFunc);
+svgImage.addEventListener('click',redirect)
 
 
-function toggleFunc(e) {
+function toggleIcon(e) {
     e.target.classList.toggle('fa-times')
 }
 
@@ -34,6 +45,39 @@ window.addEventListener('load',showCards);
 function showCards(){
     cards.forEach(element=>{
         element.style.opacity='100';
-        console.log(element)
     })
 }
+
+for (let i = 0; i < panel.length+1; i++) {
+
+    //close project panel1
+closePanel[i].addEventListener('click',function () {
+    panel[i].classList.add('panel-hide')
+    panel[i].style.zIndex = -1;
+    main.classList.remove('blur');
+    footer.classList.remove('blur');
+});
+//open project detail 1
+ReadMoreBtn[i].addEventListener('click',function() {
+    panel[i].classList.remove('panel-hide');
+    main.classList.add('blur');
+    // body.classList.add('blur');
+    footer.classList.add('blur');
+    panel[i].style.zIndex = 5;
+});
+
+    
+}
+
+
+
+
+
+
+//redirect to index.html
+function redirect() {
+    location.replace("index.html")
+  }
+
+
+ 
